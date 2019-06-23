b791e5b9f160b0f4d7c79b01eeaa1b4994c09690
Danilo Vitoriano <vitoriano08@gmail.com>
2019-06-23 19:06:17 -0300

    http get request api

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const url = require('url');
 let fs = require('fs');
+const https = require("https");
+const API = "https://jsonplaceholder.typicode.com/posts/1";
+
 
 function render(path, response) {
     fs.readFile(path, null, function (error, data) {
@@ -28,6 +31,20 @@ module.exports = {
                 break;
             case '/about':
                 render('./about.html', response);
+                break;
+            case '/api':
+                let body = "";
+                https.get(API, res => {
+                    res.setEncoding("utf8");
+                    res.on("data", data => {
+                        body += data;
+                    });
+                    res.on("end", () => {
+                        body = JSON.parse(body);
+                        console.log(body);
+                    });
+                });
+                render('./index.html', response)
                 break;
             default:
                 response.writeHead(404);
