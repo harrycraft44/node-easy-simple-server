3c5d666ab4bff7d3b7422f51bb9833e3c0ec93db
BuildTools <harrycraft11@outlook.com>
2021-11-19 19:57:24 +0000

    updated to allow more pages faster

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+var fs = require('fs');
+
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/html');
+
+    switch (req.url){
+        case "/":
+            res.end(fs.readFileSync("index.html", "utf8"));
+            break;
+        
+    }
+
 });
 
 server.listen(port, hostname, () => {
